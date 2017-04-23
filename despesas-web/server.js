73df7c28a1319fad16da2e6c1d8e0bc6e29ec66c
Fabricio Vallim <fmvallim@gmail.com>
2017-04-23 19:32:28 -0300

    Customizando a mensagem de log de startup do servidor

--- despesas-web/server.js
+++ despesas-web/server.js
@@ -5,5 +5,5 @@ var app = require('./config/express')();
 var port = yargs.port || app.get('port');
 
 http.createServer(app).listen(port, function() {
-	console.log('Express Server escutando na porta ' + port);
+	console.log('Hi! Seu servidor está no ar, escutando na porta ' + port);
 })
