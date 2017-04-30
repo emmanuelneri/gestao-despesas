018cb0f73c478a4ad1403a1ad55dd2bcde3e1b8d
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2017-04-30 19:42:52 -0300

    criado script para popular a base de dados

---
+++ scripts/popular-base.sql
@@ -0,0 +1,9 @@
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 20, 'Almoço', 'ALIMENTACAO', true);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 500, 'Pós graduação', 'EDUCACAO', true);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 10, 'Onibus', 'TRANSPORTE', true);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 2000, 'Celular', 'OUTROS', true);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 100, 'Luz', 'MORARIA', false);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 70, 'Agua', 'MORARIA', false);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES (now(), 800, 'Aluguel', 'MORARIA', false);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES ((now() - INTERVAL '1 days'), 300, 'Plano de saúde', 'SAUDE', false);
+INSERT INTO despesa(data, valor, descricao, categoria, paga) VALUES ((now() - INTERVAL '10 days'), 650, 'Carro - Parcela 12/24', 'OUTROS', false);
